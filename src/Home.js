fb6bb407e1e4df42635b7be4f97d56d37f98f3ec
Dodo <saad9191h@gmail.com>
2023-01-24 06:14:51 -0600

    completed Home section

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,30 @@
-import { Grid, Typography, Stack, Box } from "@mui/material";
+import { Grid, Typography, Stack } from "@mui/material";
 
 import MongoLogo from './logos/mongodb-icon.svg'
 import ExpressLogo from './logos/expressjs-icon.svg';
 import ReactLogo from './logos/reactjs-icon.svg';
 import NodeLogo from './logos/nodejs-icon.svg';
 
-import { grey } from '@mui/material/colors';
-
-// const color = red[500];
 
 
 
 function Home() {
     return (
-        <Grid container bgcolor={grey[400]} py={5}>
+        <Grid container py={5} sx={{backgroundColor: '#525252', color: '#fafafa'}}>
 
 
             <Grid item xs={12} sm={6} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-                <Typography variant='h2' align='center'>Saad Hussain</Typography>
-                <Typography variant='h6' align='center'>MERN Stack Developer</Typography>
+                <Typography variant='h2' align='center' sx={{fontFamily: "Playball, cursive"}}>Saad Hussain</Typography>
+                <Typography variant='body1' align='center' mb={2}>MERN Stack/JavaScript Developer</Typography>
             </Grid>
 
             <Grid item xs={12} sm={6}>
-                <Stack alignItems="center" justifyContent="center" spacing={5} mb={5} direction={{ xs: 'column', sm: 'row' }}>
+                <Stack alignItems="center" justifyContent="center" spacing={3} mb={3} direction={{ xs: 'column', sm: 'row' }}>
                     <img src={MongoLogo} alt="MongoDB logo" className="mern-svg rotate-vert-center" />
                     <img src={ExpressLogo} alt="Express logo" className="mern-svg rotate-vert-center" />
                 </Stack>
 
-                <Stack alignItems="center" justifyContent="center" spacing={2}  direction={{ xs: 'column', sm: 'row' }}>
+                <Stack alignItems="center" justifyContent="center" spacing={3}  direction={{ xs: 'column', sm: 'row' }}>
                     <img src={ReactLogo} alt="React logo" className="mern-svg rotate-vert-center" />
                     <img src={NodeLogo} alt="Node logo" className="mern-svg rotate-vert-center" />
                 </Stack>
