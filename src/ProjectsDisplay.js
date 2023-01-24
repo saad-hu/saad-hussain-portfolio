70eb08af17dcdf5899da569087715e78930fba55
Dodo <saad9191h@gmail.com>
2023-01-24 09:35:21 -0600

    added more elemets to projects array

--- src/ProjectsDisplay.js
+++ src/ProjectsDisplay.js
@@ -5,13 +5,15 @@ import DataObjectIcon from '@mui/icons-material/DataObject';
 import OpenInBrowserIcon from '@mui/icons-material/OpenInBrowser';
 
 import WeatherAppImage from './images/weather-demo-0.png';
+import YoutubeCloneImage from './images/youtube-clone.png';
+
 
 
 
 const projects = [
     { name: 'Weather App', image: WeatherAppImage, techUsed: ['HTML', 'CSS', 'JavaScript'], githubRepo: 'https://github.com/saad-hu/project-weather-app', demoLink: 'https://saad-hu.github.io/project-weather-app/' },
     { name: 'Weather App', image: WeatherAppImage, techUsed: ['HTML', 'CSS', 'JavaScript'], githubRepo: 'https://github.com/saad-hu/project-weather-app', demoLink: 'https://saad-hu.github.io/project-weather-app/' },
-    { name: 'Weather App', image: WeatherAppImage, techUsed: ['HTML', 'CSS', 'JavaScript'], githubRepo: 'https://github.com/saad-hu/project-weather-app', demoLink: 'https://saad-hu.github.io/project-weather-app/' }
+    { name: 'YouTube Clone', image: YoutubeCloneImage, techUsed: ['HTML', 'CSS'], githubRepo: 'https://github.com/saad-hu/bcp-youtube-page', demoLink: 'https://saad-hu.github.io/bcp-youtube-page/' }
 ]
 
 
@@ -32,52 +34,53 @@ const ProjectsDisplay = () => {
         <Box p={6} mb={6} sx={{ backgroundColor: '#f5f5f5' }}>
             <Grid container spacing={3}>
 
+                {projects.map(project => (
+                    <Grid item xs={12} sm={6} md={4} xl={3}>
 
-                <Grid item xs={12} sm={6} md={4} xl={3}>
+                        <Paper elevation={16}>
+                            <Card sx={{ backgroundColor: '#d4d4d4' }}>
 
-                    <Paper elevation={16}>
-                        <Card sx={{ backgroundColor: '#d4d4d4' }}>
+                                <CardMedia
+                                    component="img"
+                                    height="180"
+                                    image={project.image}
+                                />
 
-                            <CardMedia
-                                component="img"
-                                height="180"
-                                image={projects[0].image}
-                            />
+                                <CardContent>
+                                    <Typography textAlign='center' variant="h4" mb={2} className='fontPoppins' sx={{ color: '#262626' }}>{projects[0].name}</Typography>
+                                    <Stack spacing={1} direction='row' justifyContent='center'>
+                                        {project.techUsed.map((tech, index) => (
+                                            <Button variant="text" size='small' key={index} sx={{ color: '#1e3a8a' }} startIcon={<DataObjectIcon />}>{tech}</Button>
+                                        ))}
+                                    </Stack>
 
-                            <CardContent>
-                                <Typography textAlign='center' variant="h4" mb={2} className='fontPoppins' sx={{ color: '#262626' }}>{projects[0].name}</Typography>
-                                <Stack spacing={1} direction='row' justifyContent='center'>
-                                    {projects[0].techUsed.map((tech, index) => (
-                                        <Button variant="text" size='small' key={index} sx={{ color: '#1e3a8a' }} startIcon={<DataObjectIcon />}>{tech}</Button>
-                                    ))}
-                                </Stack>
+                                </CardContent>
 
-                            </CardContent>
+                                <CardActions sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
 
-                            <CardActions sx={{display: 'flex', justifyContent: 'center', gap: 2}}>
+                                    <Tooltip title='Github Repository'>
+                                        <Link href={project.githubRepo} sx={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }} >
+                                            <IconButton component="label" onMouseEnter={addRotationClass} onMouseLeave={removeRotationClass}>
+                                                <GitHubIcon sx={{ fontSize: 30 }} />
+                                            </IconButton>
+                                        </Link>
+                                    </Tooltip>
 
-                                <Tooltip title='Github Repository'>
-                                    <Link href={projects[0].githubRepo} sx={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }} >
-                                        <IconButton component="label" onMouseEnter={addRotationClass} onMouseLeave={removeRotationClass}>
-                                            <GitHubIcon sx={{ fontSize: 30 }} />
-                                        </IconButton>
-                                    </Link>
-                                </Tooltip>
+                                    <Tooltip title='Live Demo'>
+                                        <Link href={project.demoLink} sx={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }} >
+                                            <IconButton component="label" onMouseEnter={addRotationClass} onMouseLeave={removeRotationClass}>
+                                                <OpenInBrowserIcon sx={{ fontSize: 30 }} />
+                                            </IconButton>
+                                        </Link>
 
-                                <Tooltip title='Live Demo'>
-                                    <Link href={projects[0].demoLink} sx={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }} >
-                                        <IconButton component="label" onMouseEnter={addRotationClass} onMouseLeave={removeRotationClass}>
-                                            <OpenInBrowserIcon sx={{ fontSize: 30 }} />
-                                        </IconButton>
-                                    </Link>
-
-                                </Tooltip>
-                            </CardActions>
-                        </Card>
-                    </Paper>
+                                    </Tooltip>
+                                </CardActions>
+                            </Card>
+                        </Paper>
 
 
-                </Grid>
+                    </Grid>
+                ))}
 
 
 
