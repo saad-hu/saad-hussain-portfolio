5cf546582a294d78297eb81a806cc7b690c66fb6
Saad Hussain <saad9191h@gmail.com>
2023-09-11 23:52:03 +0500

    navbar initialized

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,216 @@
+import { useState, useLayoutEffect, useRef } from 'react';
 
-import { Box } from "@mui/material"
+// MUI
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-const Navbar = () => {
+// icons
+import AdbIcon from '@mui/icons-material/Adb';
+
+import "./navbar.scss"
+
+import gsap from 'gsap';
+
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBarWithMenuOnly() {
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    let timeline = useRef();
+
+    useLayoutEffect(() => {
+
+        let context = gsap.context(() => {
+
+            timeline.current = gsap.timeline()
+                .from(".navbar", {
+                    delay: 0.5,
+                    duration: 1,
+                    y: "-120%",
+                    ease: "bounce"
+                })
+        })
+
+        return () => context.revert();
+    }, [])
+
     return (
-        <Box sx={{ color: "white" }}>
-            Navbar
-        </Box>
-    )
-}
+        <>
+            <AppBar
+                position="fixed"
+                className='navbar'
+                sx={{
+                    backgroundColor: "var(--primary-bg)",
+                }}
+            >
+                <Container maxWidth="xl" className='navbar-container'>
+                    <Toolbar disableGutters>
+                        {/* the below AdbIcon(logo) and Typography(company name) are hidden in xs and md screens. this is for the screens above md */}
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
 
-export default Navbar
+                        {/* this box contains the pages buttons/links for screens above md.  */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    // onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+
+
+
+                        {/* this Box contains the hamburger icon and menu for screens below md. upon clicking 'nav' menu opens */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            {/* the 'nav' menu */}
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+
+
+                        {/* the below AdbIcon(logo) and Typography(company name) are hidden in screens above md. this is for the screens below md */}
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+
+
+
+
+
+                        {/* this Box contains the user icon for every screen. upon clicking 'user' menu opens */}
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" />
+                                </IconButton>
+                            </Tooltip>
+
+                            {/* the 'user' menu */}
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+
+            </AppBar>
+        </>
+    );
+}
+export default ResponsiveAppBarWithMenuOnly;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,10 +5,13 @@ import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar/Navbar"
 // import Testing from "../Testing"
 
+import { Toolbar } from "@mui/material"
+
 const HomePage = () => {
     return (
         <div>
             <Navbar />
+            <Toolbar />
             <Hero />
 
             {/* <Testing /> */}
