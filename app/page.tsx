53ab31b78d4849e440c783f080749909a5de35b7
Saad Hussain <saad9191h@gmail.com>
2025-06-12 12:31:04 +0500

    added shadcn accordion component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -9,9 +10,16 @@ export default function Home() {
 
       <section className="section-border-divider">
         <div className="section-container section-padding">
+          <Experience />
+        </div>
+      </section>
+
+      <section className="section-border-divider">
+        <div className="section-container section-padding">
           <Projects />
         </div>
       </section>
+
 
     </div>
   );
