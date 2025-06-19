62993a533101933042d6571a2041dc9ed8485853
Saad Hussain <saad9191h@gmail.com>
2025-06-19 05:14:35 +0500

    toaster component added using react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
+import ToasterWrapper from "@/components/ToasterWrapper";
 
 // Primary font for headings and accents
 const poppins = Poppins({
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${poppins.variable} ${roboto.variable} antialiased`}
       >
         {children}
+        <ToasterWrapper />
       </body>
     </html>
   );
---
+++ components/ToasterWrapper.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import toast, { ToastBar, Toaster } from "react-hot-toast";
+
+const ToasterWrapper = () => {
+    return (
+        <Toaster
+            position="top-right"
+            toastOptions={{
+                style: {
+                    backgroundColor: "var(--background-secondary)",
+                    color: "var(--text-secondary)"
+                },
+            }}
+        >
+            {(t) => (
+                <ToastBar toast={t}>
+                    {({ icon, message }) => (
+                        <div className="flex gap-0.5 relative">
+                            {icon}
+                            {message}
+                            {t.type !== 'loading' && (
+                                <button
+                                    className="border border-white px-1 py-0.5 rounded-full text-[10px]/3 text-center absolute top-[-12px] right-[-12px] hover:bg-[theme(--color-primary-dark)] cursor-pointer"
+                                    onClick={() => toast.dismiss(t.id)}
+                                >
+                                    X
+                                </button>
+                            )}
+                        </div>
+                    )}
+                </ToastBar>
+            )}
+        </Toaster>
+    )
+}
+
+export default ToasterWrapper
