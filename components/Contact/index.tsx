b0d9ca42099611d4a7275228bee0804629908034
Saad Hussain <saad9191h@gmail.com>
2025-06-19 05:49:05 +0500

    minor enhancements in contact section

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -8,9 +8,9 @@ const Contact = () => {
             <h2 className='section-heading mb-7'>Let's Connect</h2>
 
 
-            <div className='flex gap-5 flex-col lg:flex-row'>
+            <div className='flex gap-5 flex-col lg:flex-row items-stretch lg:items-center'>
 
-                <div className="lg:flex-1 flex flex-row items-center justify-center h-[50vh] sm:h-[75vh] lg:h-[40rem] relative w-full">
+                <div className="lg:flex-1 flex flex-row items-center justify-center h-[50vh] sm:h-[75vh] lg:h-[30rem] xl:h-[40rem] relative w-full">
                     <GlobeWrapper />
                 </div>
 
